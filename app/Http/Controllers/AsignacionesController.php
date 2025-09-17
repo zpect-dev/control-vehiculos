08e659245bd38d423ab143090af99b1cda148d09
zpect <95605545+zpect-dev@users.noreply.github.com>
2025-09-17 12:40:39 -0400

    Correcion bug asignaciones

--- app/Http/Controllers/AsignacionesController.php
+++ app/Http/Controllers/AsignacionesController.php
@@ -30,8 +30,8 @@ class AsignacionesController extends Controller
         ]);
 
         // Verifica que el nuevo kilometraje no sea menor al anterior
-        $ultimoKilometraje = HistorialAsignaciones::where('vehiculo_id', $vehiculo->placa)->orderByDesc('fecha_asignacion')->first();
-        if ($ultimoKilometraje > $validatedData['kilometraje']) return back()->with('fail', 'Kilometraje invalido');
+        $registro = HistorialAsignaciones::where('vehiculo_id', $vehiculo->placa)->orderByDesc('fecha_asignacion')->first();
+        if ($registro->kilometraje > $validatedData['kilometraje']) return back()->with('fail', 'Kilometraje invalido');
         
         // Verifica que el nuevo usuario existe
         $nuevoUsuario = User::find($request->user_id);
@@ -57,7 +57,6 @@ class AsignacionesController extends Controller
         $vehiculo->save();
         
         NotificacionHelper::emitirAsignacionUsuario($vehiculo->placa, $admin->name, $nuevoUsuario->name);
-
         return back()->with('success', 'Usuario asignado correctamente.');
     }
 
